7dd0de2cd64ba9242d9bf0541691448ab86c0551
Toby Keegan <Toby.Keegan@ibm.com>
2020-07-31 11:10:12 +0100

    Add test auth function to root URL

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+// import passport from "passport";
+import authenticate from "../authenticate";
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+routes.get("/", async (req, res) => {
+  const user = req.get("kiosk-id");
+  const auth = await authenticate(
+    req.get("kiosk-id") || "undefined",
+    req.get("api-key") || "undefined"
+  );
+
+  if (auth) {
+    res.status(200).json({ detectedUser: `Welcome, ${user}` });
+  } else {
+    res.status(403).json({ message: "Unauthorised access denied." });
+  }
 });
 
 export default routes;
