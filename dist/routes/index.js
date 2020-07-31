63978f5ef8e705411d7d4aaa2ae7e1d0c7187fba
Toby Keegan <Toby.Keegan@ibm.com>
2020-07-31 12:06:49 +0100

    Build changes

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,8 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// import passport from "passport";
+const authenticate_1 = __importDefault(require("../authenticate"));
 const routes = express_1.default.Router();
-routes.get('/', (req, res) => {
-    res.status(200).json({ message: 'Connected!' });
+routes.get('/', async (req, res) => {
+    const user = req.get('kiosk-id');
+    const auth = await authenticate_1.default(req.get('kiosk-id') || 'undefined', req.get('api-key') || 'undefined');
+    if (auth) {
+        res.status(200).json({ detectedUser: `Welcome, ${user}` });
+    }
+    else {
+        res.status(403).json({ message: 'Unauthorised access denied.' });
+    }
 });
 exports.default = routes;
