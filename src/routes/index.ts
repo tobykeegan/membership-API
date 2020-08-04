5d838999b5ea87b4297a16c16a99d18a98c0f71b
Toby Keegan <Toby.Keegan@ibm.com>
2020-08-04 16:22:14 +0100

    Fix HTTP status codes, add transaction endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import getUser from '../dbfunctions/read/getUser';
+import transaction from '../dbfunctions/update/transaction';
 
 const routes = express.Router();
 
@@ -46,6 +47,51 @@ routes.get('/user/:id', async (req, res) => {
     } else {
         // reject noauth kiosks
         res.status(403).json({ message: 'Unauthorised access denied.' });
+    }
+});
+
+/**
+ *  INCREMENT user balance
+ */
+routes.get('/user/:id/transaction', async (req, res) => {
+    if (req.auth) {
+        const id = req.params.id as string;
+        const value = req.body.value as number;
+
+        try {
+            const user = await getUser(id); // get original values for user
+
+            if (user.balance + value >= 0) {
+                // simple check if the transaction can proceed
+                await transaction(id, value) // call a MongoDB transaction
+                    .then((data) => {
+                        res.status(200).json({
+                            balance: data.value.balance.toFixed(2),
+                        });
+                    })
+                    .catch((err) => {
+                        res.status(400).json({
+                            message: 'An unexpected error has occured.',
+                            failedID: id,
+                            error: err,
+                        });
+                    });
+            } else {
+                res.status(403).json({
+                    user: user,
+                    message: 'Insufficient funds available.',
+                });
+            }
+        } catch (err) {
+            res.status(400).json({
+                message: 'An unexpected error has occured.',
+                failedID: id,
+                error: err,
+            });
+        }
+    } else {
+        // reject noauth kiosks
+        res.status(401).json({ message: 'Unauthorised access denied.' });
     }
 });
 
