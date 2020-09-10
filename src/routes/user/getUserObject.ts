b2a3dca15e89ddb6aabcf5b261ce3385197a8077
Toby Keegan <toby.keegan@ibm.com>
2020-09-10 17:47:00 +0100

    Update endpoint to handle new user model per spec

--- src/routes/user/getUserObject.ts
+++ src/routes/user/getUserObject.ts
@@ -1,7 +1,14 @@
+/* eslint-disable no-console */
 import express from 'express';
 import getUser from '../../dbfunctions/read/getUser';
 
 const routes = express.Router();
+
+function validateString(inp: string) {
+    const pattern = /[a-zA-Z0-9]{16}/;
+    console.log(pattern.test(inp));
+    return pattern.test(inp);
+}
 
 /**
  *  GET User Object
@@ -9,22 +16,29 @@ const routes = express.Router();
 export default routes.get('/api/user/:id', async (req, res) => {
     if (req.auth) {
         // return user json data
+
         try {
+            if (!validateString(req.params.id)) throw Error;
             await getUser(req.params.id)
                 .then((user) => {
-                    res.status(200).json(user);
+                    res.status(200).json({
+                        action: 'showWelcome',
+                        content: `Welcome, ${user.empFirstName}!`,
+                    });
                 })
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 .catch((err) => {
+                    res.location('/api/register/:id');
                     res.status(404).json({
                         message: 'User not found.',
                         failedID: req.params.id,
                     });
                 });
         } catch (err) {
+            console.log(err);
             res.status(400).json({
                 message:
-                    'The ID provided was invalid. It must be a string of 12 bytes.',
+                    'The ID provided was invalid. It must be a string of 16 alphanumeric characters.',
                 failedID: req.params.id,
             });
         }
