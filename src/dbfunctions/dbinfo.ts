4b718cf24425d9ced50bc4d7c58438fc133d174f
Toby Keegan <toby.keegan@ibm.com>
2020-09-10 17:49:10 +0100

    Prettier sweep on file

--- src/dbfunctions/dbinfo.ts
+++ src/dbfunctions/dbinfo.ts
@@ -2,4 +2,8 @@ import mongodb from 'mongodb';
 
 export const client = mongodb.MongoClient;
 
-export const db = `mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb/db`;
+//export const db = `mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb/db`;
+
+export const db =
+    process.env.DB_CONN_STRING ||
+    `mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb/db`;
