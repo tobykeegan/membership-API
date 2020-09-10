34d47074038b4a6bb93d883188c47dedfcd20d5b
Toby Keegan <toby.keegan@ibm.com>
2020-09-10 17:51:19 +0100

    Compatability update uses new empID instead of _id

--- src/dbfunctions/read/getUser.ts
+++ src/dbfunctions/read/getUser.ts
@@ -1,10 +1,12 @@
 import { client, db } from '../dbinfo';
-import { ObjectId } from 'mongodb';
+// import { ObjectId } from 'mongodb';
+
 
 export default async (searchedUser: string): Promise<User> => {
+    
     const conn = await client.connect(db);
     return (await conn
         .db('db')
         .collection('users')
-        .findOne({ _id: new ObjectId(searchedUser) })) as User;
+        .findOne({ empId: searchedUser })) as User;
 };
