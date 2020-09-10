8b1a2faa227ef6a1eacdbd42c1f98815cac66693
Toby Keegan <toby.keegan@ibm.com>
2020-09-10 17:49:42 +0100

    Update to use empID instead of MongoDB _id

--- src/dbfunctions/read/getUser.ts
+++ src/dbfunctions/read/getUser.ts
@@ -2,7 +2,8 @@ import { client, db } from '../dbinfo';
 import { ObjectId } from 'mongodb';
 
 export default async (searchedUser: string): Promise<User> => {
-    return (await (await client.connect(db))
+    const conn = await client.connect(db);
+    return (await conn
         .db('db')
         .collection('users')
         .findOne({ _id: new ObjectId(searchedUser) })) as User;
